2c3ec67a088d9b5172e83b0323f33fe8edabbfd7
Kirti Pogra <88347992+Kra09-kp@users.noreply.github.com>
2024-05-30 15:20:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,7 @@ def behind_the_scenes():
         Then, the final video was compressed using ffmpeg command to reduce the file size.")
     st.write("The final video file is then displayed to the user.\
         The user can download the video file by clicking on the download button.\
-        The user can also generate a new story by entering a new title and clicking on the generate button.\
         The user can also view the behind the scenes of the story creation process by clicking on the behind the scenes button.")
-
 
 
 def main(user_input,folder_path):
@@ -33,9 +31,9 @@ def main(user_input,folder_path):
         print(prompt)
         print("*"*50)
         print(dialogue)
-    with st.spinner("Generating images..."):
+    with st.spinner("Creating images..."):
         image_path = cs.save_images(prompt,f"{folder_path}/images/")
-    with st.spinner("Generating audio..."):
+    with st.spinner("Creating audio..."):
         audio_file = cs.create_audio(' '.join(dialogue),f"{folder_path}/audio.mp3")
     with st.spinner("Creating video..."):
         video_file = cs.create_video_from_images(image_path,audio_file,f"{folder_path}/video.mp4")
@@ -61,18 +59,33 @@ with st.sidebar:
             else:
                 st.error("An error occured. Please try again.")
                 print(e)
-
-
         
 if final_file == '':
     st.caption("Please enter the title and click on 'Generate' button to create the story.")
+    st.write("Here are some sample video stories created using AI:")
+    col1,col2 = st.columns(2,gap="small")
+    with col1:
+        st.video("https://youtu.be/vsI4gWXrmqo")
+        st.caption("Prompt: Training for Work-Life Balance")
+    with col2:
+        st.video("https://youtu.be/Q-sr1V_ekPU")
+        st.caption("Prompt: Training for Allyship")
 else:
     st.caption(f"Here is the story for {story}")
     video_file = open(final_file, 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes, start_time=0)
 
+
 st.sidebar.caption("Want to know how this works?")
 if st.sidebar.button("Behind the scenes"):
     behind_the_scenes()
-# prompt = "A story on the topic of 'The Magic of Friendship'"
+
+with st.sidebar:
+    st.header("👩‍💻 **About the Creator**")
+    st.write("I am a Data Science enthusiast with a passion for solving real-world problems using data. I have experience in building machine learning models, data analysis, and data visualization. I am always eager to learn new technologies and explore new domains. I am currently looking for opportunities in Data Science and Machine Learning.")
+    st.write("Let's connect to explore opportunities, share knowledge, and collaborate on exciting projects!") 
+    st.write("🔗 **Connect with Me:**")
+    #st.write("[🌐 Portfolio](https://www.google.com/)") 
+    st.write("[📧 Email](mailto:kirtipogra@gmail.com)")
+    st.write("[📝 LinkedIn](https://www.linkedin.com/in/kirti-pogra/)")
